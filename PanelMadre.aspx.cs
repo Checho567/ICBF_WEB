42d96a7f614ec2108f286316b119db3a4819e7cf
Checho567 <140514237+Checho567@users.noreply.github.com>
2025-09-01 17:30:27 -0500

    Add files via upload

---
+++ PanelMadre.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ICBFWEB2
+{
+    public partial class PanelMadre : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["idUsuario"] != null && Session["fk_idRol"].Equals(2))
+            {
+                if (!IsPostBack)
+                {
+                    lblUser.Text = Session["nombre"].ToString();
+                }
+
+            }
+            else
+            {
+                Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                Response.Cache.SetExpires(DateTime.UtcNow.AddMinutes(-1));
+                Response.Cache.SetNoStore();
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+        protected void btnIrARegistroAsistencia_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("RegistroAsistencia.aspx");
+        }
+
+        protected void btnLogout_Click(object sender, EventArgs e)
+        {
+            Utilidades.Logout();
+        }
+    }
+}
